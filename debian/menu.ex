58a3adad62150d3aff14f8f01dad1b50ec2d22ff
Robert James Clay <jame@rocasa.us>
2013-10-28 12:51:27 -0400

    Remove currently unneeded example files from the debian/ directory.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mfreq):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mfreq" command="/usr/bin/mfreq"
--- debian/mfreq.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mfreq package
-#
-0 4	* * *	root	[ -x /usr/bin/mfreq_maintenance ] && /usr/bin/mfreq_maintenance
